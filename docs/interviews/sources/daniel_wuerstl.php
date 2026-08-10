542ddcc6df56347d003e03b3dc5ac99987d3f2e5
phiresky <phireskyde+git@gmail.com>
2026-08-10 15:51:45 +0200

    docs: archive original-language interviews

---
+++ docs/interviews/sources/daniel_wuerstl.php
@@ -0,0 +1,207 @@
+<HTML>
+
+<HEAD>
+
+	<TITLE>Robin Hood [http://www.robinhood-game.de/]</TITLE>
+
+<META NAME="author" CONTENT="Pharao und T2C, robinhood-game.de">
+<META NAME="publisher" CONTENT="Pharao und T2C">
+<META NAME="copyright" CONTENT="Pharao und T2C, robinhood-game.de">
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=ISO-8859-1">
+
+<META NAME="keywords" CONTENT="Robin Hood, robin hood, robinhood-game.de, robinhood game.de, robinhood-game, Pharao, T2C, Robin Hood Game, Maid Marian, maid marian, Robin Hood und Maid Marian">
+<META NAME="description" CONTENT="Alles über das Computerspiel Robin Hood: u.a. Previews, Charakterebeschreibungen, Bilder, Downloads...">
+<META NAME="robots" CONTENT="all">
+<META NAME="revisit-after" CONTENT="5 days">
+
+<LINK REL="stylesheet" TYPE="text/css" HREF="/db/css/styles.css">
+
+<SCRIPT LANGUAGE="JavaScript" SRC="/db/jscript/library.js"></SCRIPT>
+
+<MAP NAME="navigation">
+<AREA SHAPE="rect" COORDS="1,17,100,43" HREF="/index.php" TITLE="News">
+<AREA SHAPE="rect" COORDS="1,51,100,77" HREF="/review.php" TITLE="Review">
+<AREA SHAPE="rect" COORDS="1,86,100,112" HREF="/charaktere.php" TITLE="Charaktere">
+<AREA SHAPE="rect" COORDS="1,120,100,146" HREF="/loesung.php" TITLE="Lösung">
+<AREA SHAPE="rect" COORDS="1,154,100,180" HREF="/bilder.php?page=1" TITLE="Screenshots">
+<AREA SHAPE="rect" COORDS="1,189,100,215" HREF="/stuff.php" TITLE="Dies und jenes">
+<AREA SHAPE="rect" COORDS="1,222,100,248" HREF="/community.php" TITLE="Community">
+<AREA SHAPE="rect" COORDS="1,259,100,285" HREF="/kontakt.php" TITLE="Kontakt / Impressum">
+<AREA SHAPE="rect" COORDS="1,295,100,321" HREF="/desperados/index.php" TITLE="Zur Desperados Sektion">
+</MAP>
+
+</HEAD>
+
+<BODY BGCOLOR="#007700" MARGINWIDTH="0" MARGINHEIGHT="0" TOPMARGIN="0" LEFTMARGIN="0" RIGHTMARGIN="0" BOTTOMMARGIN="0">
+
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="900" ALIGN="center">
+	<TR>
+		<TD WIDTH="900" ALIGN="center">
+
+<!-- 'table_header' Funktion: Header -->
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="900" HEIGHT="110">
+	<TR>
+		<TD WIDTH="5" BACKGROUND="/grafiken/tree_horizontal.gif"><IMG SRC="/grafiken/tree_horizontal.gif" BORDER="0"></TD>
+		<TD WIDTH="890"><IMG SRC="/grafiken/header.gif" BORDER="0"></TD>
+		<TD WIDTH="5" BACKGROUND="/grafiken/tree_horizontal.gif"><IMG SRC="/grafiken/tree_horizontal.gif" BORDER="0"></TD>
+	</TR>
+</TABLE>
+
+<!-- 'table_site' Funktion: Seite Navigation, Content -->
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="900" HEIGHT="100%" ALIGN="center">
+<TR>
+	<TD WIDTH="5" HEIGHT="100%" BACKGROUND="/grafiken/tree_horizontal.gif"><IMG SRC="/grafiken/dummy.gif" BORDER="0"></TD>
+	<TD WIDTH="100" HEIGHT="100%" ALIGN="left" VALIGN="top" BACKGROUND="/grafiken/bgcolor.gif"><IMG SRC="/grafiken/navigation.gif" BORDER="0" USEMAP="#navigation"></br>&nbsp;</br></TD>
+	<TD WIDTH="5" HEIGHT="100%" BACKGROUND="/grafiken/bgcolor.gif" VALIGN="top"><IMG SRC="/grafiken/tree_horizontal.gif" BORDER="0" WIDTH="5" HEIGHT="405"></TD>
+	<TD WIDTH="785" HEIGHT="100%" ALIGN="left" VALIGN="top" BACKGROUND="/grafiken/bgcolor.gif"><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="785"><TR><TD WIDTH="33%" HEIGHT="1" ALIGN="left" CLASS="navi">&nbsp;</TD><TD WIDTH="33%" ALIGN="center" CLASS="headline"><B>- Daniel Würstl -</B></TD><TD WIDTH="33%" HEIGHT="1" ALIGN="right" CLASS="navi"><A HREF="/stuff.php" TITLE="Stuff">Stuff</A> &raquo; <A HREF="/stuff/interviews.php" TITLE="Stuff - Interviews">Interviews</A> &raquo; <a href="/stuff/interviews/daniel_wuerstl.php" title="Interviews - Daniel Würstl">Daniel Würstl</a>&nbsp;&nbsp;&nbsp;&nbsp;</td></tr></table><TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="785" HEIGHT="100%">
+
+<TR>
+
+	<TD WIDTH="20" HEIGHT="100%">&nbsp;</TD>
+
+	<TD WIDTH="745" HEIGHT="100%" VALIGN="top" ALIGN="left"></br>
+
+<table ID="normal">
+<tr>
+<td width="20">&nbsp;</td>
+<td>
+
+<b>robinhood-game.de:</b> <i>Der obligatorische Anfang: Kannst du uns zuerst eine kleine Einleitung zu deiner Person geben?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Jung, gerade noch ledig und direkt nach dem Studium der BWL mit Schwerpunkt "Marketing - Kommunikation" bei Spellbound gelandet.<br> 
+Meine Computerspielkarriere ist jedoch viel beeindruckender: Beginnend mit Pong auf VC20 und Lunar Lander auf C64 mit Datasette ist sie bis heute ungebrochen. Über 20 Jahre Spielerfahrung :-)</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Wie ist der momentane Status von "Chicago 1930"? Wann ist der Release-Termin, ist schon eine Demo festgesetzt und wie sieht es mit einer offiziellen Seite aus?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Einen festen Temrin gibt es noch nicht. Rechtzeitig zu Weihnachten ist es aber da. Die PC Games war neulich da und wird einen Bericht mit Videos über Chicago bringen.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Warum habt ihr euch entschieden, das "Vampire Hunter" Projekt vorerst sein zu lassen und doch lieber "Chicago 1930" zu entwickeln?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Hm, da muss du Armin fragen. Es waren wohl beides gute Ideen und man musste sich einfach für eine entscheiden.</br></br></br></br>
+
+
+<b>robinhood-game.de:</b> <i>Wie sieht es mit der Grafik aus? Bietet die aufgebohrte Version der bereits aufgebohrten Engine noch genug Möglichkeiten um mit aktuellen Titeln mitzuhalten?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Das ist meines Erachtens vor allem Geschmacksache. Wer auf gorbpixelige 3D Geschichten wegen der Dynamik steht ist mit Chicago natürlich weniger glücklich als jemand, der das Detail und die augenschmeichelnden Grafiken sucht. Die gibt es in Chicago und ihr werdet überrascht sein, wie abwechslungsreich Räume in den 30er eingerichtet waren :-)</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Martin Kuppe hat mal gesagt, dass die Mehrheit der Missionen innerhalb von Gebäuden spielen wird. Wie müssen wir uns das vorstellen? Ist das ganze Level dann ein einziges großes Gebäude, oder ist es ein Ausschnitt aus einem Stadtviertel, das überwiegend aus großen, begehbaren Häusern besteht? Warum habt ihr euch entschlossen, mehr Gebäude zu integrieren?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Meistens ist das Level ein großes Gebäude. Nur weil das Level grundsätzlich im Gebäude ist, heißt das übrigens nicht, das man nicht von der Umgebung sieht: durch die Ansicht ist meistens auch die Außenmauer sichtbar und immer ist es möglich, durch offene Fenster an den Fenstersimsen entlangzuklettern... Die Gebäude bringen viel Abwechsung in das Spiel: immer nur dunkle Hinterhöfe wäre ja langweilig. So gibt es Motels und Hotels, einen spektakulären Hauptbahnhof, Bordelle uvm.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Zur Story: Kannst du uns schon ein paar mehr Details über die Story verraten als "Die Mafia muss die Stadt erobern, die Cops müssen sie wieder zurückerobern"?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Nein sorry, dazu müsst ihr es selber spielen. Oder möchtet ihr immer schon vorher das Ende der Geschichte erfahren?</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Wird es wieder einen linearen oder einen nicht-linearen Spielverlauf geben? Gibt es infolge dessen wieder mehr oder weniger Videos zwischen den Missionen?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Meistens bietet sich die Auswahl verschiedener Missionen an, was von strategischer Bedeutung ist. Es macht beispielsweise einen Unterschied, ob ich zuerst das Viertel mit dem Trainingscenter oder das mit dem Waffenlader erobere, weil das eben auch die entsprechenden Ressourcen eröffnet. Die Missionen bauen jedoch nicht direkt aufeinander auf.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Es heißt ja, dass der Spieler in "Chicago 1930", ähnlich wie in "Robin Hood", wieder Gegenstände einsammeln und seine Charaktere trainieren kann. Gibt es deshalb wieder eine Art "Hauptquartier" oder habt ihr euch was völlig neues einfallen lassen?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Ja, es gibt sozusagen ein "Stammviertel", vom dem aus die beiden kontrahierenden Parteien beginnen und letztendlich ist es das Ziel, das des Gegners zu erreichen und einzunehmen. Die Charaktere entwickeln sich im Laufe der Zeit und werden so zu einem Team aus Spezialisten, das wohlüberlegt eingesetzt werden will.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Wie ist das mit den ungefähr 30 spielbaren Charakteren? Sind diese mehr in der Art der Merry Men aus "Robin Hood" oder sind das alles Protagonisten? Warum verwendet ihr so viele Leute?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Nun es gibt auch hier einen Hauptprotagonisten, der in jedem Level mitspielt. Das restliche Team lässt sich dann aber je nach Anforderung zusammenstellen. Da es einige verschiedene Eigenschaftswerte für die Charaktere gibt, ist es sinnvoll aus mehreren wählen zu können, um einen mit einer optimalen Eigenschaftenkombination verfügbar zu haben.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Im Interview bei HomeLan heißt es, dass ihr für jeden spielbaren Charakter ein richtiges Inventar haben werdet und dass jeder so ziemlich jede Waffe nutzen kann. Trotzdem soll es ein Taktikspiel, wie wir es kennen, werden. Was tut ihr, damit "Chicago 1930" nicht zu einem schlechtem Mix aus "Jagged Alliance" und "Desperados"/"Commandos" wird?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Wir machen einen guten Mix daraus :-)</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Und nun zu Desperados 2: Es heißt, ihr arbeitet schon seit Anfang dieses Jahres an "Desperados 2: Coopers Revenge" und der vorraussichtliche Release-Termin sei Ende 2004. Stimmen diese Daten? Wie es der momentane Entwicklungsstatus?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Ende 2004 ist optimistisch gerechnet. Da es ja komplett in 3D sein wird und mit dem Vorgänger hohe Erwartungen erfüllen muss, kann es auch etwas länger dauern. Im Moment wird an der Demo für Präsentationen bei Publishern gearbeitet. Es gibt bereits einen "spielbaren" Level mit Gegnern und Ziel...</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Es gab (und gibt immernoch) sehr viel Aufregung und Verwirrung wegen der Ankündigung von "Desperados 2". Vorallem, weil Armin Gessert bereits einmal gesagt hat: "Ein Desperados 2 wird es von Spellbound definitiv nicht geben". Warum habt ihr euch dennoch dazu entschlossen, das Spiel zu entwickeln? Hat Infogrames (bzw. Atari) nun doch endlich nachgegeben?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Ach das sind alles lange und unspannende Geschichten... Wir sollten uns vielleicht einfach darüber freuen, dass man sich nun dazu entschieden hat, was?</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Habt ihr schon einen Publisher für das Spiel oder seit ihr vielleicht gerade mitten in den Verhandlungen? Wenn ja, welches Logo, außer dem von Spellbound, werden wir auf der Packung sehen?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Der Publisher will erst noch bestimmt werden...</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Die meisten Desperados bzw. Robin Hood Fans stehen einer 3D-Grafik nicht gerade freundlich gegenüber. Weshalb seit ihr von der altbewährten Methode "2D-Grafik über einem 3D-Gerüst" abgekommen?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Vor allem deshalb, weil unsere Programmierer keinen Bock mehr auf die alte Engine haben und etwas neues machen wollen. Es sind aber noch immer die gleichen Leute mit dem gleichen Ehrgeiz, ein richtig gutes Spiel zu machen, am Werk. Ihr könnt also unbesorgt sein und darauf vertrauen, dass es neben hässlichen 3D Spielen in Zukunft auch sehr ansehnliche geben wird.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Die Desperados-Fanwelt ist zur Zeit ziemlich gespalten. Die einen wollen richtig große, aber nur gute, Veränderungen, die anderen haben Angst, dass nur alles "verschlimmbessert" wird und wollen alles beim alten belassen. Wie sieht es nun wirklich aus? Habt ihr wirklich einschneidende Veränderungen geplant oder nur kleine Erweiterungen? Wie sehen diese Veränderungen/Erweiterungen in etwa aus?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Das ist im Detail noch zu früh zu sagen. Es gibt ja noch nicht einmal eine ausgearbeitete Storry. Das wichtigste, die Stimmung im Spiel, ist aber schon im ersten Demolevel zu spüren und ich bin sehr zuversichtlich, das auch D2 eine sehr dichte atmosphärische haben wird, die den Fans von D1 gefallen wird.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Wie steht es um das Gerücht, Desperados 2 könnte aus der Ego-Perspektive gespielt werden. Ist da was dran? Wenn ja, warum, wieso, weshalb?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Hier wird es eine variable Ansicht geben, die die Vorteile der Ego mit den Vorteilen einer Betrachterperspektive vereint.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Wie steht es um die Charaktere? Gibt es neue, gibt es andere, gibt es vielleicht sogar weniger? Werden die Charaktere neue Fähigkeiten haben?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Tja, die alten und wahrscheinlich auch ein paar neue.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Ein enorm wichtiges Thema: Werden für die Charaktere die gleichen Synchronsprecher wie in Teil 1 engagiert?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Da bin ich überfragt. Wüsste aber nicht, was dem entgegensteht.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Kannst du uns schon ein paar erste Details oder zumindest die grobe Richtung der Story verraten? Mich persönlich würde es sehr interessieren, warum es ausgerechnet "Coopers Revenge" heißt. Cooper will sich doch nicht etwa für das ganze Hin und Her mit den Webisodes und dem AddOn rächen, oder etwa doch?</i></br></br>
+<b>Daniel Würstl:</b></br>
+:-)</br>
+Coopers Revenge ist bisher mehr ein Arbeitstitel. Viele sind damit nicht glücklich, eben weil es nicht wirklich einen Grund zur Rache gibt...</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Habt ihr schon einen Mann, der die Musik (in "Chicago 1930" und in "Desperados 2") machen wird? [Anmerkung: Ich meine damit, ob Tapio Muley auch wieder mit von der Partie sein wird]</i></br></br>
+<b>Daniel Würstl:</b></br>
+sieht verdammt so aus</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Jetzt, da es offiziell ein Desperados 2 geben wird, wie steht ihr nun dem Thema Webisodes bzw. AddOn (für Teil 1) gegenüber. Gibt es jetzt wieder eine Chance, nun doch endlich die neuen Levels spielen zu können? Und darüber hinaus: So ziemlich alle Fans wünschen sich mal ein KLARES Statement in Bezug auf den Editor bzw. die Möglichkeit bzw. die Erlaubnis um ein paar Mods für Desperados zu erstellen. Seit mehr als einem Jahr, versuchen wir, mal mehr mal weniger intensiv, euch dieses Statement bzw. gleich den ganzen Editor zu entlocken. Wenigstens das Statement könnte doch jetzt fällig sein.</i></br></br>
+<b>Daniel Würstl:</b></br>
+Das gleiche habe ich erst mich dann, als das ergebnislos blieb, auch Armin gefragt: hier seine Originalantwort:</br></br>
+Tja, wenn Du die Rechte mit Infogrames klärst und zudem ein paar Monate das Teil so umprogrammierst, dass auch ein Normalsterblicher damit umgehen kann, wenn Du die Dokumentation dazu schreibst, und wenn Du dann die Support-Anfragen bearbeitest, können wir den Leveleditor gerne rausgeben.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Wie war die Games Convention? Hat's dir gefallen? Hat sich der eigene Stand gelohnt oder plant ihr eventuell schon für's nächste Jahr?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Ja, die ersten zwei Tage waren noch recht spannend. Dann wurde es langsam anstrengend. Aber wir waren schließlich dort, um zu arbeiten. Und das haben wir ja auch getan. 9000 Postkarten und 5000 Poster wurden verteilt und unser Stand sah wirklich seeeehr cool aus. Daher denke ich schon, dass sich die Messe irgendwann gelohnt haben wird, auch wenn so was natürlich wirtschaftlich nicht zu ermessen ist.</br></br></br></br>
+
+<b>robinhood-game.de:</b> <i>Möchtest du abschließend vielleicht noch irgendetwas ergänzen oder etwas, bis jetzt noch unerwähntes, hinzufügen?</i></br></br>
+<b>Daniel Würstl:</b></br>
+Da möchte ich nur noch einen netten Sinnspruch eines Kollegen zu eurer Erheiterung springen lassen:</br></br>
+...just because you're paranoid doesn't mean we don't follow you...</br></br></br></br>
+
+
+<i>Wir bedanken uns für das aufschlussreiche Interview. Ich denke, dass die gesamte Community jetzt schlauer als vorher ist (das auf jeden Fall, aber mir persönlich hat es wirklich gefallen und es sind einige richtig interessante Infos dabei!).</br>Danke Daniel Würstl!</i></br></br></br>
+
+
+</td>
+<td width="20">&nbsp;</td>
+</tr>
+</table>
+
+
+<BR>&nbsp;<BR>
+	</TD>
+
+	<TD WIDTH="20" HEIGHT="100%">&nbsp;</TD>
+
+</TR>
+
+</TABLE>
+
+	</TD>
+
+	<TD WIDTH="5" HEIGHT="100%" BACKGROUND="/grafiken/tree_horizontal.gif"><IMG SRC="/grafiken/dummy.gif" BORDER="0"></TD>
+
+</TR>
+
+<TR>
+
+	<TD COLSPAN="5" WIDTH="900" HEIGHT="5" BACKGROUND="/grafiken/tree_vertikal.gif"><IMG SRC="/grafiken/dummy.gif" BORDER="0"></TD>
+
+</TR>
+
+</TABLE>
+
+		</TD>
+	</TR>
+</TABLE>
+
+<BR>&nbsp;
+
+</BODY>
+
+</HTML>
